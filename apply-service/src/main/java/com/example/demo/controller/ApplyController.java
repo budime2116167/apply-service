e61b562e1cd94c73ce2c0f4e713e8018c2ff06a0
venkat <budime1205@gmail.com>
2021-12-07 22:50:43 +0000

    commit controller

---
+++ apply-service/src/main/java/com/example/demo/controller/ApplyController.java
@@ -0,0 +1,9 @@
+package com.example.demo.controller;
+
+public class ApplyController {
+	public String message() {
+		System.out.println("this is my controller class");
+	return "welcome to apply controller";
+	}
+
+}
