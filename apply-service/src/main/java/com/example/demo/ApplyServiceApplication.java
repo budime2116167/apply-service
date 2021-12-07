5a41ac3bc895f17cd23ac427063baa57bc87f4cf
venkat <budime1205@gmail.com>
2021-12-07 22:38:52 +0000

    commit first

---
+++ apply-service/src/main/java/com/example/demo/ApplyServiceApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplyServiceApplication.class, args);
+		System.out.println("welcome to spring boot project");
+		System.out.println("welcome to spring boot project");
+		System.out.println("welcome to spring boot project");
+		System.out.println("welcome to spring boot project");
+		
+	}
+
+}
